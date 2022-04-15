357a78fe5ec02ab8cce703d5ef36e4658ad418aa
helloworldrussia <coolgrifers@gmail.com>
2022-04-15 23:00:25 +0500

    stable 1.1

--- mailer/urls.py
+++ mailer/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from mailer.views import IndexView, test, client_ask, add_emails, invite
 
 urlpatterns = [
-    path('index/', IndexView.as_view(), name='home'),
+    path('', IndexView.as_view(), name='home'),
     path('test/', test),
     path('email/<str:type>/<str:email>', client_ask),
     path('download/', add_emails, name='download'),
