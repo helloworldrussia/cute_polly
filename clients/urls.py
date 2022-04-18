e5d52c1ff9788c8f06178607c49f020f1c64fa49
helloworldrussia <coolgrifers@gmail.com>
2022-04-18 23:28:42 +0500

    admin

--- clients/urls.py
+++ clients/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from clients.views import client_sub
-#dkiryaevfreelancehunt@gmail.com
+
 urlpatterns = [
     path('sub/', client_sub, name='clients_sub'),
 ]
--- cute_polly/urls.py
+++ cute_polly/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('l-cab/', admin.site.urls),
     path('', include('mailer.urls')),
     path('users/', include('accounts.urls')),
     path('clients/', include('clients.urls'))
