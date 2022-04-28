46b0f6234302ad07df2aed535d82cbd872f45f9f
helloworldrussia <coolgrifers@gmail.com>
2022-04-28 16:35:45 +0500

    new url for form

--- mailer/urls.py
+++ mailer/urls.py
@@ -5,7 +5,7 @@ from mailer.views import IndexView, client_ask, add_emails, invite, inv_form
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
-    path('invite/', inv_form, name='inv_form'),
+    path('invite-form/', inv_form, name='inv_form'),
     path('email/<str:type>/<str:email>', client_ask),
     path('download/', add_emails, name='download'),
     path('invite/', invite, name='invite'),
