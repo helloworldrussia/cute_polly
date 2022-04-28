f477d28777455831ed2e084492e0eaf353bc465b
helloworldrussia <coolgrifers@gmail.com>
2022-04-28 16:25:10 +0500

    good inv_form

--- mailer/models.py
+++ mailer/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Address(models.Model):
-    email = models.CharField(max_length=255, unique=True, null=False, db_index=True,
+    email = models.CharField(max_length=255, unique=True, db_index=True,
                              verbose_name='Адрес получателя')
     status = models.TextField(null=False, default='new', verbose_name='Статус клиента')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
@@ -10,6 +10,14 @@ class Address(models.Model):
 
     def __str__(self):
         return self.email
+
+    def clean(self):
+        if self.email == '' or self.email == ' ':
+            raise ValueError("email cant be '' or ''")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = "Адрес"
